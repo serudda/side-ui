013a8e258cb2cd12cc4fbe7bf6cc9a1563534ea3
Zyruks <kuzenshi@gmail.com>
2023-06-05 00:49:33 -0300

    feat(buttons): :sparkles: Add Copy Button component and related files

---
+++ src/components/Buttons/CopyBtn/CopyBtn.tsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+import cn from 'classnames';
+import { Button, ButtonProps, ButtonSize, ButtonVariant, IconCatalog, Tooltip, HtmlType } from '~/components';
+
+export interface CopyBtnProps extends ButtonProps {
+  /**
+   * Ref to the target element
+   */
+  target: React.RefObject<HTMLElement> ;
+  /**
+   * Specify if the tooltip is active
+   */
+  isToolTipActive: boolean;
+  isTextActive: boolean;
+}
+
+export const CopyBtn = ({
+  className,
+  invert = false,
+  isDisabled = false,
+  isIconSolid = false,
+  isToolTipActive = false,
+  size = ButtonSize.base,
+  startIcon = IconCatalog.clipboard,
+  target,
+  variant = ButtonVariant.ghost,
+  isTextActive = true,
+  ...restOfProps
+}: CopyBtnProps) => {
+  const classes = cn(className);
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    const text = target.current?.textContent;
+    if (text) {
+      navigator.clipboard.writeText(text);
+      setCopied(true);
+    }
+  };
+
+  const handleMouseLeave = () => {
+    /**
+     * Reset the copied state to false when mouse leaves the button
+     */
+    setCopied(false);
+  };
+  const tooltipText = copied ? 'Copied' :  'Copy';
+
+  const renderButton = (
+    <Button
+      invert={invert}
+      className={classes}
+      variant={variant}
+      size={size}
+      onClick={handleCopy}
+      onMouseLeave={handleMouseLeave}
+      disabled={isDisabled}
+      isIconSolid={isIconSolid}
+      startIcon={startIcon}
+      htmlType={HtmlType.button}
+      {...restOfProps}
+    >
+      {isTextActive && tooltipText}
+    </Button>
+  );
+
+  return (
+    <>
+      {isToolTipActive && (
+        <Tooltip text={tooltipText}>
+          {renderButton}
+        </Tooltip>
+      )}
+      {!isToolTipActive && renderButton}
+    </>
+  );
+
+};
