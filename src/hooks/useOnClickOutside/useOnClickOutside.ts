e1d23378b95a5195774f522e9321fe6ad9f3b377
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2023-06-17 13:06:33 -0500

    refactor: clean code

--- src/hooks/useOnClickOutside/useOnClickOutside.ts
+++ src/hooks/useOnClickOutside/useOnClickOutside.ts
@@ -13,10 +13,7 @@ export function useOnClickOutside<T extends HTMLElement = HTMLElement>(
   handler: Handler,
   mouseEvent: 'mousedown' | 'mouseup' = 'mousedown',
 ): void {
-  console.log('ref: ', ref);
-  console.log('useEventListener: ', useEventListener);
   useEventListener(mouseEvent, (event) => {
-    console.log('event: ', event);
     const el = ref?.current;
 
     // Do nothing if clicking ref's element or descendent elements
