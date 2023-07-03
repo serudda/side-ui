f3f99d50ce89f3b1e8ecc6711c34f96fd9ed98a3
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2023-07-03 16:02:46 -0500

    refactor: clean code

--- src/hooks/useLatestValue/useLatestValue.ts
+++ src/hooks/useLatestValue/useLatestValue.ts
@@ -1,6 +1,10 @@
 import { useRef } from 'react';
 import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect/useIsomorphicLayoutEffect';
 
+/**
+ * The useLatestValue hook allows you to get the latest value of a variable.
+ * @reference https://github.com/tailwindlabs/headlessui/blob/main/packages/%40headlessui-react/src/hooks/use-latest-value.ts
+ */
 export function useLatestValue<T>(value: T) {
   const cache = useRef(value);
 
--- src/hooks/useWindowEvent/useWindowEvent.ts
+++ src/hooks/useWindowEvent/useWindowEvent.ts
@@ -1,6 +1,10 @@
 import { useEffect } from 'react';
 import { useLatestValue } from '../useLatestValue/useLatestValue';
 
+/**
+ * The useWindowEvent hook allows you to add an event listener to the window.
+ * @reference https://github.com/tailwindlabs/headlessui/blob/main/packages/%40headlessui-react/src/hooks/use-window-event.ts
+ */
 export function useWindowEvent<TType extends keyof WindowEventMap>(
   type: TType,
   listener: (ev: WindowEventMap[TType]) => any,
