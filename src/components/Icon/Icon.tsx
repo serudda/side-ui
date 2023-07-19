7b0cf2a82669552016158c7e0cc0ef38001cf5ed
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2023-07-19 14:08:14 -0500

    refactor: extends icon catalog

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -17,6 +17,7 @@ export enum IconCatalog {
   clipboard = 'clipboard',
   discord = 'discord',
   documentCheck = 'documentCheck',
+  ellipsisHorizontal = 'ellipsisHorizontal',
   ellipsisHorizontalCircle = 'ellipsisHorizontalCircle',
   faceSmile = 'faceSmile',
   hashtag = 'hashtag',
@@ -112,6 +113,10 @@ const Icons: Record<IconCatalog, IconType> = {
       'M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 0 1 9 9v.375M10.125 2.25A3.375 3.375 0 0 1 13.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 0 1 3.375 3.375M9 15l2.25 2.25L15 12',
     solid:
       'M9 1.5H5.625c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0016.5 9h-1.875a1.875 1.875 0 01-1.875-1.875V5.25A3.75 3.75 0 009 1.5zm6.61 10.936a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 14.47a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z M12.971 1.816A5.23 5.23 0 0114.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 013.434 1.279 9.768 9.768 0 00-6.963-6.963z',
+  },
+  [IconCatalog.ellipsisHorizontal]: {
+    outline:
+      'M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0zm6 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0zm6 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0z',
   },
   [IconCatalog.ellipsisHorizontalCircle]: {
     outline:
