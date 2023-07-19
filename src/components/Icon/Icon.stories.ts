1718598eb33285f8202523090482a992ad58a53c
Samuel Llibre Santos <kuzenshi@gmail.com>
2023-07-19 02:08:47 -0300

    feat: :sparkles: Updated Icon component and story with new props and styles

--- src/components/Icon/Icon.stories.ts
+++ src/components/Icon/Icon.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { parseIconOptions } from '@/storybook/utils';
-import { Icon, IconCatalog } from './Icon';
+import { Icon, IconCatalog, IconStyle } from './Icon';
 
 const meta = {
   title: 'UI Components/Icon',
@@ -24,6 +24,7 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     icon: IconCatalog.adjustmentsHorizontal,
+    iconStyle: IconStyle.regular,
     className: 'w-6 h-6',
   },
 };
