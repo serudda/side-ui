b83f2927c3ef37c06c55cafee181459c999537d7
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2023-09-29 16:21:48 -0500

    refactor: define a new component named "StackedAvatar" and "CountAvatar"

--- src/components/Avatars/CountAvatar/CountAvatar.tsx
+++ src/components/Avatars/CountAvatar/CountAvatar.tsx
@@ -1,13 +1,20 @@
 import { cn } from '@common';
-import { Avatar, AvatarSize, Sizes as AvatarSizes, type AvatarProps } from '@components';
+import { AvatarSize, AvatarSizes, type AvatarProps } from '@components';
 
-const LabelSizes: Record<AvatarSize, string> = {
-  [AvatarSize['2xs']]: 'text-[10px]',
-  [AvatarSize.xs]: 'text-xs',
-  [AvatarSize.sm]: 'text-sm',
-  [AvatarSize.base]: 'text-sm',
-  [AvatarSize.lg]: 'text-base',
-  [AvatarSize.xl]: 'text-lg',
+export enum CountAvatarSize {
+  xs = 'xs',
+  sm = 'sm',
+  base = 'base',
+  lg = 'lg',
+  xl = 'xl',
+}
+
+const LabelSizes: Record<CountAvatarSize, string> = {
+  [CountAvatarSize.xs]: 'text-xs',
+  [CountAvatarSize.sm]: 'text-sm',
+  [CountAvatarSize.base]: 'text-sm',
+  [CountAvatarSize.lg]: 'text-base',
+  [CountAvatarSize.xl]: 'text-lg',
 };
 
 export interface CountAvatarProps extends AvatarProps {
@@ -17,28 +24,19 @@ export interface CountAvatarProps extends AvatarProps {
 /**
  * This component is used to display a count avatar to show the number of remaining avatars
  */
-export const CountAvatar = ({
-  className,
-  count = 0,
-  size = AvatarSize.base,
-  ...restProps
-}: CountAvatarProps) => {
+export const CountAvatar = ({ className, count = 0, size = AvatarSize.base }: CountAvatarProps) => {
   const classes = {
-    container: cn('flex items-center justify-center relative overflow-hidden', className),
-    countContainer: cn(
-      'absolute z-[1] flex items-center justify-center bg-black rounded-full',
+    container: cn(
+      'flex items-center justify-center relative align-middle bg-slate-200 dark:bg-black rounded-full',
       AvatarSizes[size],
+      className,
     ),
-    count: cn('text-slate-50 font-semibold', LabelSizes[size]),
+    count: cn('text-slate-950 dark:text-slate-50 font-semibold', LabelSizes[size]),
   };
 
   return (
     <div className={classes.container}>
-      <div className={classes.countContainer}>
-        <span className={classes.count}>+{count}</span>
-      </div>
-
-      <Avatar size={size} {...restProps} />
+      <span className={classes.count}>+{count}</span>
     </div>
   );
 };
--- src/components/Avatars/StackedAvatar/StackedAvatar.tsx
+++ src/components/Avatars/StackedAvatar/StackedAvatar.tsx
@@ -1,25 +1,31 @@
 import { Children, cloneElement, isValidElement } from 'react';
 import { cn } from '@common';
-import { AvatarSize, CountAvatar } from '@components';
+import { AvatarBorder, AvatarSize, CountAvatar } from '@components';
 
 const DEFAULT_MAX_AVATARS = 5;
 
-export const PositiveMargins: Record<AvatarSize, string> = {
-  [AvatarSize['2xs']]: 'ml-2',
-  [AvatarSize.xs]: 'ml-2.5',
-  [AvatarSize.sm]: 'ml-3',
-  [AvatarSize.base]: 'ml-3.5',
-  [AvatarSize.lg]: 'ml-4',
-  [AvatarSize.xl]: 'ml-5',
+export enum StackedAvatarSize {
+  xs = 'xs',
+  sm = 'sm',
+  base = 'base',
+  lg = 'lg',
+  xl = 'xl',
+}
+
+export const PositiveMargins: Record<StackedAvatarSize, string> = {
+  [StackedAvatarSize.xs]: 'ml-1',
+  [StackedAvatarSize.sm]: 'ml-1.5',
+  [StackedAvatarSize.base]: 'ml-2',
+  [StackedAvatarSize.lg]: 'ml-2.5',
+  [StackedAvatarSize.xl]: 'ml-3',
 };
 
-export const NegativeMargins: Record<AvatarSize, string> = {
-  [AvatarSize['2xs']]: '-ml-2',
-  [AvatarSize.xs]: '-ml-2.5',
-  [AvatarSize.sm]: '-ml-3',
-  [AvatarSize.base]: '-ml-3.5',
-  [AvatarSize.lg]: '-ml-4',
-  [AvatarSize.xl]: '-ml-5',
+export const NegativeMargins: Record<StackedAvatarSize, string> = {
+  [StackedAvatarSize.xs]: '-ml-1',
+  [StackedAvatarSize.sm]: '-ml-1.5',
+  [StackedAvatarSize.base]: '-ml-2',
+  [StackedAvatarSize.lg]: '-ml-2.5',
+  [StackedAvatarSize.xl]: '-ml-3',
 };
 
 export interface StackedAvatarProps {
@@ -27,6 +33,8 @@ export interface StackedAvatarProps {
    * Specify an optional className to be added to the component
    */
   className?: string;
+
+  border?: AvatarBorder;
 
   maxAvatars?: number;
 
@@ -40,13 +48,17 @@ export interface StackedAvatarProps {
  */
 export const StackedAvatar = ({
   className,
+  border,
   maxAvatars = DEFAULT_MAX_AVATARS,
   size = AvatarSize.base,
   children,
 }: StackedAvatarProps) => {
   const classes = {
     container: cn('flex', className, PositiveMargins[size]),
-    avatar: cn('block align-middle', NegativeMargins[size]),
+    avatar: (className: string) => cn('align-middle', NegativeMargins[size], className),
+    count: cn('align-middle', NegativeMargins[size], {
+      'ring-2 ring-slate-300 dark:ring-slate-800 ring-offset-0': border,
+    }),
   };
 
   const childrenArray = Children.toArray(children);
@@ -56,15 +68,16 @@ export const StackedAvatar = ({
 
     if (remaining < 1) return null;
 
-    return <CountAvatar count={remaining} size={size} className={classes.avatar} />;
+    return <CountAvatar count={remaining} size={size} className={classes.count} />;
   };
 
   const newChildren = childrenArray.slice(0, maxAvatars).map((child) => {
     if (!isValidElement(child)) return null;
 
     return cloneElement(child, {
-      className: classes.avatar,
+      className: classes.avatar(child.props.className),
       size,
+      border,
       ...child.props,
     });
   });
