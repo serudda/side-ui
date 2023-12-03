1a2343f2f07e6b866d9a27d32caa27f1407761c0
Sergio Ruiz Davila <sergioruizdavila@gmail.com>
2023-12-03 17:14:52 -0500

    refactor: change the Modal structure

--- src/components/Overlays/Modals/BaseModal/BaseModal.tsx
+++ src/components/Overlays/Modals/BaseModal/BaseModal.tsx
@@ -1,55 +1,32 @@
-import { ReactNode, useEffect } from 'react';
-import { Key, cn } from '@common';
-import { useBodyClass, useKeyPress } from '@hooks';
+import { ReactNode } from 'react';
+import cn from 'classnames';
+import { ModalContent, ModalSize } from '../ModalSections';
+import { ModalBodyProps } from '../ModalSections/ModalBody/ModalBody';
+import { ModalFooterProps } from '../ModalSections/ModalFooter/ModalFooter';
+import { ModalHeaderProps } from '../ModalSections/ModalHeaders';
 
-export enum ModalSize {
-  sm = 'sm',
-  base = 'base',
-  lg = 'lg',
-  xl = 'xl',
-  fullScreen = 'fullScreen',
-}
-
-const Sizes: Record<ModalSize, string> = {
-  [ModalSize.sm]: 'max-w-md',
-  [ModalSize.base]: 'max-w-lg',
-  [ModalSize.lg]: 'max-w-xl',
-  [ModalSize.xl]: 'max-w-2xl',
-  [ModalSize.fullScreen]: 'max-w-full',
-};
-
-export enum ModalBgColor {
-  transparent = 'transparent',
-  slate = 'slate',
-  black = 'black',
-}
-
-const BgColors: Record<ModalBgColor, string> = {
-  [ModalBgColor.transparent]: 'bg-transparent',
-  [ModalBgColor.slate]: 'bg-slate-950',
-  [ModalBgColor.black]: 'bg-black',
-};
-
-export interface BaseModalProps {
+export interface BaseModalProps
+  extends Omit<ModalBodyProps, 'children'>,
+    Omit<ModalFooterProps, 'children'> {
   /**
    * Specify an optional className to be added to the component
    */
   className?: string;
 
   /**
-   * Changes the size of the modal, giving it more or less padding
+   * Specify an optional className to be added to the body section.
    */
-  size?: ModalSize;
+  bodyClassName?: string;
 
   /**
-   * Changes the background color of the modal
+   * Specify an optional id to be added to the primary action button.
    */
-  bgColor?: ModalBgColor;
+  primaryActionBtnId?: string;
 
   /**
    * Define the modal header
    */
-  header?: ReactNode;
+  header?: Omit<ModalHeaderProps, 'onCloseBtnClick'>;
 
   /**
    * Elements to display inside the Modal.
@@ -62,80 +39,60 @@ export interface BaseModalProps {
   footer?: ReactNode;
 
   /**
+   * Changes the size of the modal, giving it more or less padding
+   */
+  size?: ModalSize;
+
+  /**
+   * Indicates if the footer must be shown, default to `true`
+   */
+  showFooter?: boolean;
+
+  /**
    * The callback to get notified when the modal was closed
    */
   onClose?: () => void;
 }
 
 /**
- * Modals focus the user’s attention exclusively on
- * one task or piece of information via a window that sits on top of the page content.
+ * Modals focus the user’s attention exclusively on one task or piece of information via a window that sits on top of the page content.
+ * @author Sergio Ruiz<sergioruizdavila@gmail.com>
+ * Created at 2022-08-02
  */
 export const BaseModal = ({
   className,
-  size = ModalSize.base,
-  bgColor = ModalBgColor.slate,
   header,
   body,
   footer,
+  size = ModalSize.base,
   onClose,
+  bodyClassName,
+  showFooter = true,
+  isBodyScrollable = true,
+  primaryActionBtnId,
 }: BaseModalProps) => {
-  const classes = {
-    container: cn(
-      'transform-none transition-transform relative w-auto pointer-events-none',
-      Sizes[size],
-      {
-        'my-7 mx-auto h-full': size !== ModalSize.fullScreen,
-        'h-screen': size === ModalSize.fullScreen,
-      },
-      className,
-    ),
-    content: cn(
-      'overflow-hidden',
-      'relative flex flex-col w-full pointer-events-auto bg-clip-padding outline-0',
-      BgColors[bgColor],
-      {
-        'rounded-2xl max-h-full': size !== ModalSize.fullScreen,
-        'h-full': size === ModalSize.fullScreen,
-      },
-    ),
-    header: cn({
-      'p-3': !header,
-    }),
-    body: cn('relative', {
-      'flex-auto overflow-y-auto px-6 scrollbar-w-2 scrollbar-thumb-rounded-lg scrollbar-thumb-slate-800 scrollbar-track-slate-900':
-        size !== ModalSize.fullScreen,
-    }),
-    footer: cn('flex flex-wrap flex-shrink-0 items-center w-full', {
-      'p-6': footer,
-      'p-3': !footer,
-    }),
-  };
-
-  useBodyClass('opened-modal');
-  const pressedEsc = useKeyPress(Key.Escape);
+  const classes = cn(className);
 
   const handleCancelBtnClick = () => {
     if (onClose) onClose();
   };
 
-  useEffect(() => {
-    if (pressedEsc) handleCancelBtnClick();
-  }, [pressedEsc]);
-
   /* Render JSX */
   return (
-    <div className={classes.container}>
-      <div className={classes.content}>
-        {/* HEADER */}
-        <div className={classes.header}>{header}</div>
-
-        {/* BODY */}
-        <div className={classes.body}>{body}</div>
-
-        {/* FOOTER */}
-        <div className={classes.footer}>{footer}</div>
-      </div>
-    </div>
+    <ModalContent
+      primaryActionBtnId={primaryActionBtnId}
+      className={classes}
+      size={size}
+      onEscKeyPress={handleCancelBtnClick}
+    >
+      {/* HEADER */}
+      {header && <ModalContent.Header {...header} onClose={handleCancelBtnClick} />}
+      {/* BODY */}
+      <ModalContent.Body className={bodyClassName} isBodyScrollable={isBodyScrollable}>
+        {body}
+      </ModalContent.Body>
+      {/* FOOTER */}
+      {showFooter && <ModalContent.Footer>{footer}</ModalContent.Footer>}
+    </ModalContent>
   );
 };
