867820dfed76814736279dc3a3ed0b15f792b738
Samuel Llibre Santos <kuzenshi@gmail.com>
2024-02-17 15:43:45 -0400

    fix(tailwind): 🐛 darkMode is bug without adding darkMode in config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   //presets: [require('./tailwind-presets/storybook-preset')],
+  darkMode: 'class',
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   plugins: [require('tailwindcss-scrollbar')],
 };
