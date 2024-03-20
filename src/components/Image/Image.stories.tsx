4332470f3df8a3ece4236577ffebbe31f0c7f330
Samuel Llibre Santos <kuzenshi@gmail.com>
2024-03-20 19:16:48 -0300

    chore(storybook): ♻️ update story and add image

--- src/components/Image/Image.stories.tsx
+++ src/components/Image/Image.stories.tsx
@@ -1,5 +1,5 @@
-import type { Meta, StoryObj } from '@storybook/react';
 import { Image } from './Image';
+import type { Meta, StoryObj } from '@storybook/react';
 
 const meta = {
   title: 'UI Components/Image',
@@ -13,5 +13,38 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     src: 'assets/images/default-avatar.svg',
+  },
+};
+
+export const WithFallback: Story = {
+  args: {
+    src: 'BROKEN/default-avatar.svg',
+    fallbackImage: 'assets/images/alt-avatar.svg',
+    alt: 'Fallback Avatar',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'Demonstrates using a fallback image when the primary image source fails.',
+      },
+    },
+  },
+};
+
+export const CustomErrorState: Story = {
+  render: (args) => (
+    <Image
+      src="BROKEN/default-avatar.svg"
+      alt="Broken Image"
+      noImg={<div style={{ color: 'red', fontSize: '20px' }}>Image not available</div>}
+      {...args}
+    />
+  ),
+  parameters: {
+    docs: {
+      description: {
+        story: 'Shows a custom JSX element when both the primary and fallback images fail to load.',
+      },
+    },
   },
 };
